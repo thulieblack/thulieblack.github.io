d8974b0705d7b8e4a1e576bb43446414fb5cc69b
thulieblack <sibanda.thulie@gmail.com>
2025-08-11 22:34:47 +0200

    fix-tags

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -6,14 +6,25 @@ import { allBlogs } from 'contentlayer/generated'
 import tagData from 'app/tag-data.json'
 import { genPageMetadata } from 'app/seo'
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
 const POSTS_PER_PAGE = 5
 
-export async function generateMetadata(props: {
+export const generateStaticParams = async () => {
+  const tagCounts = tagData as Record<string, number>
+
+  return Object.keys(tagCounts).map((tag) => ({
+    tag: slug(tag),
+  }))
+}
+
+export async function generateMetadata({
+  params,
+}: {
   params: Promise<{ tag: string }>
 }): Promise<Metadata> {
-  const params = await props.params
-  const tag = decodeURI(params.tag)
+  const resolvedParams = await params
+  const tag = decodeURI(resolvedParams.tag)
   return genPageMetadata({
     title: tag,
     description: `${siteMetadata.title} ${tag} tagged content`,
@@ -26,26 +37,27 @@ export async function generateMetadata(props: {
   })
 }
 
-export const generateStaticParams = async () => {
-  const tagCounts = tagData as Record<string, number>
-  const tagKeys = Object.keys(tagCounts)
-  return tagKeys.map((tag) => ({
-    tag: encodeURI(tag),
-  }))
-}
-
-export default async function TagPage(props: { params: Promise<{ tag: string }> }) {
-  const params = await props.params
-  const tag = decodeURI(params.tag)
+export default async function TagPage({ params }: { params: Promise<{ tag: string }> }) {
+  const resolvedParams = await params
+  const tag = decodeURI(resolvedParams.tag)
+  // Capitalize first letter and convert space to dash
   const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
   const filteredPosts = allCoreContent(
     sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
   )
-  const totalPages = Math.ceil(filteredPosts.length / POSTS_PER_PAGE)
-  const initialDisplayPosts = filteredPosts.slice(0, POSTS_PER_PAGE)
+
+  if (filteredPosts.length === 0) {
+    return notFound()
+  }
+
+  const pageNumber = 1
+  const initialDisplayPosts = filteredPosts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
   const pagination = {
-    currentPage: 1,
-    totalPages: totalPages,
+    currentPage: pageNumber,
+    totalPages: Math.ceil(filteredPosts.length / POSTS_PER_PAGE),
   }
 
   return (
